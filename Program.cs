f61a24e1f87b24f32b0c8c4a0768165a4bf33848
Elvis Justino da SIlva <elvis.justino@hotmail.com>
2021-11-17 15:08:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CSVCleaner
+{
+    public class Program
+    {
+        private const string PATH_IN = "In";
+        private const string PATH_OUT = "Out";
+
+        static void Main(string[] args)
+        {
+            WriteConsole("Iniciando CSV Cleaner", true);
+            WriteConsole("Preparando diretorios", true);
+            WriteConsole("Criando pasta de entrada (In)");
+            Directory.CreateDirectory(PATH_IN);
+            WriteConsole("Criando pasta de saída (Out) \n");
+            Directory.CreateDirectory(PATH_OUT);
+
+            var filePaths = Directory.GetFiles(PATH_IN, "*.csv");
+            var qtFile = filePaths.Length;
+            if (qtFile > 0)
+            {
+                WriteConsole($"{qtFile} arquivo(s) encontrado(s)! Iniciando Limpeza!", true);
+                foreach (var path in filePaths)
+                {
+                    //Pegar arquivo
+                    WriteConsole($"Arquivo: {Path.GetFileName(path)}");
+                    WriteConsole("1 - Regatando texto!");
+                    string csvText = File.ReadAllText(path);
+                    //Limpar
+                    WriteConsole("2 - Limpando o texto do arquivo!");
+                    var cleanedText = Cleaner(csvText);
+                    //Criando arquivo limpo
+                    WriteConsole("3 - Gerando arquivo de saida!\n");
+                    var outPath = $"{PATH_OUT}/{Path.GetFileName(path)}";
+                    File.AppendAllTextAsync(outPath, cleanedText);
+                }
+            }
+            else
+            {
+                WriteConsole("\nNenhum arquivo encontrado! Finalizando...");
+            }
+        }
+
+        private static void WriteConsole(string text, bool isTitle = false)
+        {
+            if (isTitle)
+            {
+                Console.WriteLine("-----" + text + "-----\n");
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+
+        }
+        public static string Cleaner(string input)
+        {
+            if (input == null || input.Equals(string.Empty)) return string.Empty;
+
+            var output = input.Trim()
+                    .Replace("&", "e")
+                    .Replace("á", "a")
+                    .Replace("é", "e")
+                    .Replace("í", "i")
+                    .Replace("ó", "o")
+                    .Replace("ú", "u")
+                    .Replace("ý", "y")
+                    .Replace("à", "a")
+                    .Replace("è", "e")
+                    .Replace("ì", "i")
+                    .Replace("ò", "o")
+                    .Replace("ù", "u")
+                    .Replace("â", "a")
+                    .Replace("ê", "e")
+                    .Replace("î", "i")
+                    .Replace("ô", "o")
+                    .Replace("û", "u")
+                    .Replace("ã", "a")
+                    .Replace("õ", "o")
+                    .Replace("ç", "c");
+
+            return output;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# CSVCleaner
+
+Projeto com o objetivo simples de substituir letras com acentuações (ex: ç, é, í), por letras sem acentuações de um arquivo ".csv".
+
+## 🚀 Começando
+
+ - Colocar o arquivo "*.csv" na pasta In.
+ - Executar o CSVCleaner.
+ - O arquivo limpo será gerado na pasta Out.
+
+### 📋 Pré-requisitos
+
+SDK .Net 5.0: https://dotnet.microsoft.com/download/dotnet/5.0
